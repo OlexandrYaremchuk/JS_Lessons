b50eb4f7fce4ac3c6b4f247e659a7ee07601f9a7
OlexandrYaremchuk <70215770+OlexandrYaremchuk@users.noreply.github.com>
2023-02-04 23:43:49 +0200

    as

--- src/App.js
+++ src/App.js
@@ -1,40 +1,41 @@
-import './App.css';
-import 'animate.css'
-import React, { Component } from 'react';
-import { BrowserRouter, Routes, Route, NavLink, Link, Router } from 'react-router-dom';
-import Navbar from './Components/Navbar/Navbar';
-import Header from './Components/Header/Header';
-import M1L1 from './Components/Lessons/M1L1';
-import M1L2 from './Components/Lessons/M1L2';
-import GitHub from './Components/Lessons/GitHub';
+import "./App.css";
+import "animate.css";
+import React, { Component } from "react";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  NavLink,
+  Link,
+  Router,
+  HashRouter,
+} from "react-router-dom";
+import Navbar from "./Components/Navbar/Navbar";
+import Header from "./Components/Header/Header";
+import M1L1 from "./Components/Lessons/M1L1";
+import M1L2 from "./Components/Lessons/M1L2";
+import GitHub from "./Components/Lessons/GitHub";
 
 function App(props) {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <div className="App">
         <Header />
-        <div className='Main'>
-          <div className='navbarBox'>
+        <div className="Main">
+          <div className="navbarBox">
             <Navbar />
-
           </div>
 
-          <div className='mainTextBox'>
+          <div className="mainTextBox">
             <Routes>
-              <Route path='GitHub' element={<GitHub />} />
-              <Route path='M1L1' element={<M1L1 />} />
-              <Route path='M1L2' element={<M1L2 />} />
-
+              <Route path="GitHub" element={<GitHub />} />
+              <Route path="M1L1" element={<M1L1 />} />
+              <Route path="M1L2" element={<M1L2 />} />
             </Routes>
           </div>
-
         </div>
-
-
-
       </div>
-
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
